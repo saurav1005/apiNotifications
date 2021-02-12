781ecf4ce204d55e9e6b66ea3a269a49d309dbfc
saurav <saurav1005@outlook.com>
2021-02-12 17:26:23 +1100

    Input validations handling and tests

--- api/config/index.js
+++ api/config/index.js
@@ -11,3 +11,21 @@ exports.LOG_LEVELS = logLevels;
 
 exports.DEFAULT_LOG_LEVEL =
   process.env.DEFAULT_LOG_LEVEL || this.LOG_LEVELS.INFO;
+
+const httpStatusCodes = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  TECHNIAL_ERROR: 500,
+};
+exports.HTTP_STATUSCODES = httpStatusCodes;
+
+const errorMessages = {
+  ERROR_INVALIDBODY: 'Request Body is not valid',
+  ERROR_INVALIDTOEMAIL: 'Email Id(s) in To is invalid',
+  ERROR_INVALIDCCEMAIL: 'Email Id(s) in Cc is invalid',
+  ERROR_INVALIDBCCEMAIL: 'Email Id(s) in BCc is invalid',
+  ERROR_INVALIDSUBJECT: 'Email Subject is Mandatory',
+};
+
+exports.ERROR_INPUTINVALID = errorMessages;
--- api/utils/logger.js
+++ api/utils/logger.js
@@ -1,11 +1,13 @@
 //TODO Placeholder implementation of modifying logs with date-timestamp and invoking via interceptors
 const { LOG_LEVELS, DEFAULT_LOG_LEVEL } = require('../config');
 
-exports.log = (message, file) => {
-  if (message && file) {
-    console.log(`${file} | ${message}`);
+exports.log = (message, file, method) => {
+  if (message && file && method) {
+    console.log(`[${new Date()}] ${file} | ${method} | ${message} `);
+  } else if (message && file) {
+    console.log(`[${new Date()}] ${file} | ${message} `);
   } else if (message) {
-    console.log(`${message}`);
+    console.log(`[${new Date()}] ${message} `);
   }
 };
 
